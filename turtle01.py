153f6f646e4c3eb7933961c0f8c2dba81edf321f
brukvo <smullerin@gmail.com>
2017-07-19 15:01:05 +0900

    turtle #commit

---
+++ turtle01.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+import turtle
+
+turtle.circle(20)
+answer = ''
+while answer != ('N' and 'n' and 'Н' and 'н'):
+    answer = turtle.textinput("Python 3.x + Turtle", "Нарисовать ещё одну окружность? (Y/N/Д/Н)")
+    if answer == ('Y' and 'y' and 'Д' and 'д'):
+        turtle.circle(30)
+    if answer == ('N' and 'n' and 'Н' and 'н'):
+        print("Спасибо за запуск демонстрации связки Python 3.x и Turtle!")
+    else:
+        print("Аварийное завершение - пользователь ввёл неверное значение.")
