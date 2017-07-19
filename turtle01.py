eda76a3b4f1c4897e466eab05202b8f93719a2c5
brukvo <smullerin@gmail.com>
2017-07-19 17:29:41 +0900

    making drawing process more obvious

--- turtle01.py
+++ turtle01.py
@@ -3,13 +3,33 @@
 import turtle
 import random
 
+print("Устанавливаем скорость рисования")
+turtle.speed(1)
+print("Рисуем окружность радиусом 20 точек")
 turtle.circle(20)
 answer = ''
 while answer != 'N':
     answer = turtle.textinput("Python 3.x + Turtle", "Нарисовать ещё несколько разных окружностей? (Y/N/Д/Н)")
     if answer == 'Y':
+        print("Выбираем случайное число для цвета")
+        brt = random.random()
         for iterate in range(20):
-            turtle.circle(random.randrange(10,100,2))
+            print("Поднимаем перо")
+            turtle.penup()
+            print("Передвигаемся в новую точку")
+            turtle.goto(random.randrange(0, 200), random.randrange(0, 200))
+            print("Опускаем перо")
+            turtle.pendown()
+            print("Устанавливаем цвет заливки")
+            turtle.fillcolor(0, random.random(), 0)
+            print("Устанавливаем цвет линии")
+            turtle.pencolor(0, random.random(), 0)
+            turtle.begin_fill()
+            print("Рисуем окружность №" + str(iterate) + ", которая будет заполнена выбранным нами цветом")
+            turtle.circle(random.randrange(10, 100, 10))
+            print("Делаем заливку выбранным случайным образом цветом")
+            turtle.end_fill()
+        print("Закончили рисование")
     elif answer == 'N':
         pass
     else:
