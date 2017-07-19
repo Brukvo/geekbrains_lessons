21e81c905099eb00ef0825c2ed1d787ffde4dacd
brukvo <smullerin@gmail.com>
2017-07-19 16:13:27 +0900

    draw 20 different circles with random (10 to 100 with step of 2) radiuses

--- turtle01.py
+++ turtle01.py
@@ -6,9 +6,12 @@ import random
 turtle.circle(20)
 answer = ''
 while answer != 'N':
-    answer = turtle.textinput("Python 3.x + Turtle", "Нарисовать ещё одну окружность? (Y/N/Д/Н)")
+    answer = turtle.textinput("Python 3.x + Turtle", "Нарисовать ещё несколько разных окружностей? (Y/N/Д/Н)")
     if answer == 'Y':
-        turtle.circle(random.randrange(10,200,2))
+        for iterate in range(20):
+            turtle.circle(random.randrange(10,100,2))
+    elif answer == 'N':
+        pass
     else:
         print("Неправильный ввод!")
 print("Спасибо за запуск демонстрации связки Python 3.x и Turtle!")
