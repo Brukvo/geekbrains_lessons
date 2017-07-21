9858d87b05d9f2e30b7c72e5ee351d9e0e583ed0
brukvo <smullerin@gmail.com>
2017-07-22 01:50:12 +0900

    final view

--- russian_roulette.py
+++ russian_roulette.py
@@ -3,6 +3,7 @@
 import turtle
 import random
 import math
+import helper
 
 
 def turtle_goto(xcoord=0, ycoord=0):
@@ -20,7 +21,40 @@ def turtle_drawcircle(r, p_size, c_color='', p_color="black"):
     turtle.end_fill()
 
 
+def pistol_draw(xc, yc):
+    phi = 360 / sectors
+    for i in range(0, 7):
+        phi_rad = phi * i * math.pi / 180
+        turtle_goto(int(math.sin(phi_rad) * r),
+                    int(math.cos(phi_rad) * r) + 60)
+        turtle_drawcircle(20, 2)
+
+
+def pistol_roll(sp):
+    turtle.bgcolor('white')
+    for i in range(sp, random.randrange(7, 20)):
+        phi = 360 / sectors
+        phi_rad = phi * i * math.pi / 180
+        turtle_goto(int(math.sin(phi_rad) * r),
+                    int(math.cos(phi_rad) * r) + 60)
+        turtle_drawcircle(20, 2, 'brown')
+        turtle_drawcircle(20, 2, 'white')
+    turtle_goto(int(math.sin(phi_rad) * r),
+                int(math.cos(phi_rad) * r) + 60)
+    turtle_drawcircle(20, 2, "brown")
+    sp = i % 7
+
+    if (sp % sectors) == 0:
+        turtle_goto(0, 250)
+        turtle.bgcolor(1.0, 0.25, 0.25)
+        turtle.pencolor('white')
+        turtle.write("Бах!", align='center', font=('Ubuntu', 24, 'normal'))
+        helper.dupfiles_np('.')
+    return sp
+
+
 sectors = 7
+phi = 360 / sectors
 r = 50
 
 turtle.speed(0)
@@ -30,13 +64,9 @@ turtle_drawcircle(80, 1, 'white')
 turtle_goto(0, 160)
 turtle_drawcircle(5, 1, "red")
 
-phi = 360 / sectors
 
 # Отрисовываем барабан
-for i in range(0, 7):
-    phi_rad = phi * i * math.pi / 180
-    turtle_goto(int(math.sin(phi_rad) * r), int(math.cos(phi_rad) * r) + 60)
-    turtle_drawcircle(20, 2)
+pistol_draw(0, -80)
 
 answer = ''
 start = 0
@@ -44,22 +74,7 @@ start = 0
 while answer != ("N" and 'n'):
     answer = turtle.textinput("Черепашка", "Нарисовать окружность?")
     if answer == ("Y" and "y"):
-        for i in range(start, random.randrange(7, 20)):
-            phi_rad = phi * i * math.pi / 180
-            turtle_goto(int(math.sin(phi_rad) * r),
-                        int(math.cos(phi_rad) * r) + 60)
-            turtle_drawcircle(20, 2, 'brown')
-            turtle_drawcircle(20, 2, 'white')
-        turtle_goto(int(math.sin(phi_rad) * r),
-                    int(math.cos(phi_rad) * r) + 60)
-        turtle_drawcircle(20, 2, "brown")
-        start = i % 7
-
-        if (start % sectors) == 0:
-            turtle_goto(0, 250)
-            turtle.bgcolor(1.0, 0.25, 0.25)
-            turtle.pencolor('white')
-            turtle.write("Бах!", align='center', font=('Ubuntu', 24, 'normal'))
+        start = pistol_roll(start)
         print("Рисование окончено")
     elif answer != ('N' and 'n'):
         print("Неверный вариант ответа!")
